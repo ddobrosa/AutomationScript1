7f711fe9acf22a09874d3396a396295e37721343
Danny Dobrosak <daniel.dobrosak@ausnetservices.com.au>
2023-09-27 15:19:12 +1000

    Added Hi 123

--- AutomationScript_1/AutomationScript_1.cs
+++ AutomationScript_1/AutomationScript_1.cs
@@ -17,7 +17,7 @@ namespace AutomationScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+			engine.GenerateInformation("Hi 123..!");
 		}
 	}
 }
